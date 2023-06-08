53058b930b586cf122654954997745c77cc0036d
Omrtyurke123 <mrtrky814@gmail.com>
2023-06-08 17:56:27 +0300

    updat

---
+++ main.js
@@ -0,0 +1,76 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   
+  });
+
+
+  var swiper = new Swiper(".coming-contenr", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+var sss= document.querySelector(".why")
+sss.style.display="flex"
+// var sss2= document.querySelector(".why2")
+// sss2.style.display="flex"
+var sss3= document.querySelector(".why3")
+sss3.style.display="flex"
+
+
+// var swiper =new Swiper(".coming-contenr",{  
+// spaceBetween:20,
+//   loop:true,
+//   autoplay:{
+//     delay:55000,
+//     disableOnInteraction : false,
+//   },
+//   centeredSlides:true,
+//   breakpoints:{
+//     0:{
+//         slidesperView :2,
+//     },
+//     568:{
+//         slidesperView :3 ,
+//     },
+//     768:{
+//         slidesperView:4,
+// },
+// 968:{
+//     slidesperView:5,
+// },
+//   },
+// }
+// );
+// if(window.scrollY > 260){
+//   setTimeout(()=>{  let header = document.querySelector("header")
+//   header.style.backgroundColor="#eee"},1000)
+// }
+
+
+
+
+
+
+
